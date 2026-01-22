9f05d1f66067e1b23ee2ef36b9e7ee85da253bda
Nguyen Quan <quandlstb@gmail.com>
2026-01-22 23:30:16 +0700

    Nguyen Quan update User

--- src/main/java/Entity/User.java
+++ src/main/java/Entity/User.java
@@ -11,7 +11,7 @@ public class User { //custormer has same meaning with usser
     //Minh Hoa
     //Constructor with .... elements
     public User(){ 
-
+        
     }
 
     //Minh Hoa
@@ -20,16 +20,31 @@ public class User { //custormer has same meaning with usser
     }
 
     static List<Booking> viewBookings(){
+        /*
+        Khuc nay la viewBooking thi lam sao de thay dc nhung phong minh dat?
+        Ta can phai truy cap vao database
+        Ham connect da duoc xay dung hay tim cach select
+        Khi select duoc ket qua tao 1 bien List<Booking> truyen danh sach do vao bien nay
+        Luc nay ta return lai thoi
+        */
       return null;
     }
 
     //Minh Hoa
-    static Booking makeBooking(){
-        return null;
+    static boolean makeBooking(){
+        /*
+        O day cung can truy cap vao db 1 phan
+        sau khi truy cap ta se dung ham update cai ngay thang checkIn va checkOut
+        update xong thi return gia tri da tao duoc hay chua
+        */
+        return true;
     }
 
     //Minh Hoa
     static boolean cancleBooking(int BookingID){
+        /*
+        Giong tren kia
+        */
         return true;
     }
 }
