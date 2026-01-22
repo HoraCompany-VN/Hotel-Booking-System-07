132ea390db7d1f192e19066ee577ce70cb0f7852
Nguyen Quan <quandlstb@gmail.com>
2026-01-22 23:25:03 +0700

    Huong dan

--- src/main/java/Entity/Hotel.java
+++ src/main/java/Entity/Hotel.java
@@ -8,13 +8,21 @@ public class Hotel {
 
     //Trinh
     //Constructor
-    public Hotel(){
-
+    public Hotel(int hotelID, String nameHotel, String phone){
+        this.hotelID = hotelID;
+        this.name = nameHotel;
+        this.numberPhone = phone;
     }
 
     //Trinh
-    static void getAvailableRoom(Room input){
-        
+    static void getAvailableRoom(){
+        /*
+        Tai day can tao 1 thang Room 
+        Room co kha nang ket noi voi ben kia can quan tam toi cai Room.status
+        Dieu quan trong o day la can viec Select sao cho tim nhung room status = availible
+        nen la truoc mat can tim hieu cach truyen lenh va viec lenh dua tren file
+        DatabaseControl.java
+        */
     }
 
 }
